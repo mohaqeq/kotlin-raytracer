d1bd1c32bf5854544838b66d06656c42a306e949
Hamidreza Mohaqeq <hamidreza.mohaqeq@scalable.capital>
2023-08-02 09:49:57 +0200

    Refactor code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -38,13 +38,3 @@ fun main() {
         }
     }
 }
-
-fun Ray.color(world: Iterable<Hittable>): Color {
-    val hit = world.hit(this, 0.0, Double.MAX_VALUE)
-    if (hit.first) {
-        val normal = hit.second!!.normal
-        return 0.5 * (normal + Color(1.0, 1.0, 1.0))
-    }
-    val blendFactor = 0.5 * (direction.unit().y + 1.0)
-    return (1 - blendFactor) * Color(1.0, 1.0, 1.0) + blendFactor * Color(0.5, 0.7, 1.0)
-}
--- src/main/kotlin/Ray.kt
+++ src/main/kotlin/Ray.kt
@@ -1,3 +1,13 @@
 data class Ray(val origin: Point, val direction: Vector) {
     fun at(t: Double) = origin + t * direction
+
+    fun color(world: Iterable<Hittable>): Color {
+        val hit = world.hit(this, 0.0, Double.MAX_VALUE)
+        if (hit.first) {
+            val normal = hit.second!!.normal
+            return 0.5 * (normal + Color(1.0, 1.0, 1.0))
+        }
+        val blendFactor = 0.5 * (direction.unit().y + 1.0)
+        return (1 - blendFactor) * Color(1.0, 1.0, 1.0) + blendFactor * Color(0.5, 0.7, 1.0)
+    }
 }
