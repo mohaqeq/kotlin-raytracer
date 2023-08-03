13848429dabad7d0e42ad8f3674fc226e40b403e
Hamidreza Mohaqeq <hamidreza.mohaqeq@scalable.capital>
2023-08-03 15:38:21 +0200

    Refactor

--- src/main/kotlin/Hittable.kt
+++ src/main/kotlin/Hittable.kt
@@ -1,20 +1,20 @@
 interface Hittable {
-    fun hit(ray: Ray, tMin: Double, tMax: Double): Pair<Boolean, Collision?>
+    fun hit(ray: Ray, tMin: Double, tMax: Double): CollisionResult
 }
 
 fun Iterable<Hittable>.hit(ray: Ray, tMin: Double, tMax: Double): CollisionResult {
     var hitAnything = false
     var closest = tMax
-    var hit: Collision? = null
+    var collision: Collision? = null
 
     for (item in this) {
         val itemHit = item.hit(ray, tMin, closest)
-        if (itemHit.first) {
+        if (itemHit.collided) {
             hitAnything = true
-            closest = itemHit.second!!.t
-            hit = itemHit.second
+            closest = itemHit.collision!!.t
+            collision = itemHit.collision
         }
     }
 
-    return CollisionResult(hitAnything, hit)
+    return CollisionResult(hitAnything, collision)
 }
--- src/main/kotlin/Sphere.kt
+++ src/main/kotlin/Sphere.kt
@@ -1,24 +1,24 @@
 import kotlin.math.sqrt
 
 data class Sphere(val center: Point, val radius: Double, val material: Material) : Hittable {
-    override fun hit(ray: Ray, tMin: Double, tMax: Double): Pair<Boolean, Collision?> {
+    override fun hit(ray: Ray, tMin: Double, tMax: Double): CollisionResult {
         val oc = ray.origin - center
         val a = ray.direction.lengthSquared()
         val b = oc dot ray.direction
         val c = oc.lengthSquared() - radius * radius
 
         val discriminant = b * b - a * c
-        if (discriminant < 0) return Pair(false, null)
+        if (discriminant < 0) return CollisionResult(false, null)
         val sqrtD = sqrt(discriminant)
 
         // Find the nearest root that lies in the acceptable range.
         var root = (-b - sqrtD) / a
         if (root < tMin || tMax < root) {
             root = (-b + sqrtD) / a
-            if (root < tMin || tMax < root) return Pair(false, null)
+            if (root < tMin || tMax < root) return CollisionResult(false, null)
         }
 
         val point = ray.at(root)
-        return Pair(true, Collision(point, root, material, ray.direction, (point - center) / radius))
+        return CollisionResult(true, Collision(point, root, material, ray.direction, (point - center) / radius))
     }
 }
