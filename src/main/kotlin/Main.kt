8dcd5af45d9ed7de1728ac48ca1c0ce98a8a7e98
Hamidreza Mohaqeq <hamidreza.mohaqeq@scalable.capital>
2023-08-04 14:55:13 +0200

    Add concurrency in rendering

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 import java.io.File
+import java.util.Collections
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 import kotlin.math.sqrt
 import kotlin.random.Random
 
@@ -18,23 +21,28 @@ fun main() {
     val lookFrom = Point(13.0, 2.0, 3.0)
     val lookAt = Point(0.0, 0.0, 0.0)
     val camera = Camera(
-        lookFrom, lookAt, Vector(0.0, 1.0, 0.0), 20.0, aspectRatio, 0.1, 10.0
+        lookFrom, lookAt, Vector(0.0, 1.0, 0.0), 25.0, aspectRatio, 0.1, 10.0
     )
 
     // Render
+    val executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
     fileWriter.use { writer ->
         writer.println("P3\n${imageWidth} ${imageHeight}\n256")
         for (j in imageHeight - 1 downTo 0) {
             print("\rScanlines remaining: $j  ")
             for (i in 0 until imageWidth) {
-                val pixelColor = Color()
+                val pixels = Collections.synchronizedList(mutableListOf<Color>())
+                val result = mutableListOf<Future<*>>()
                 for (s in 0 until pixelSamples) {
-                    val u = (i.toDouble() + Random.nextDouble()) / (imageWidth - 1)
-                    val v = (j.toDouble() + Random.nextDouble()) / (imageHeight - 1)
-                    val ray = camera.getRay(u, v)
-                    pixelColor += ray.color(world, maxDeath)
+                    result.add(executor.submit {
+                        val u = (i.toDouble() + Random.nextDouble()) / (imageWidth - 1)
+                        val v = (j.toDouble() + Random.nextDouble()) / (imageHeight - 1)
+                        val ray = camera.getRay(u, v)
+                        pixels.add(ray.color(world, maxDeath))
+                    })
                 }
-                pixelColor /= pixelSamples.toDouble()
+                result.map { it.get() }
+                val pixelColor = pixels.reduce { x, y -> x + y } / pixelSamples.toDouble()
                 val gammaCorrectedColor = Color(sqrt(pixelColor.x), sqrt(pixelColor.y), sqrt(pixelColor.z))
                 writer.println(gammaCorrectedColor.toColorString())
             }
@@ -56,12 +64,12 @@ fun randomScene(): Iterable<Hittable> {
             if ((center - Point(4.0, 0.2, 0.0)).length() > 0.9) {
                 val sphereMaterial: Material
 
-                if (chooseMat < 0.8) {
+                if (chooseMat < 0.55) {
                     // diffuse
                     val albedo = Color.random() * Color.random()
                     sphereMaterial = Lambertian(albedo)
                     world.add(Sphere(center, 0.2, sphereMaterial))
-                } else if (chooseMat < 0.95) {
+                } else if (chooseMat < 0.80) {
                     // metal
                     val albedo = Color.random(0.5, 1.0)
                     val fuzz = Random.nextDouble(0.0, 0.5)
@@ -69,7 +77,7 @@ fun randomScene(): Iterable<Hittable> {
                     world.add(Sphere(center, 0.2, sphereMaterial))
                 } else {
                     // glass
-                    sphereMaterial = Dielectric(1.5)
+                    sphereMaterial = Dielectric(1.5, Color.random(0.7, 1.0))
                     world.add(Sphere(center, 0.2, sphereMaterial))
                 }
             }
